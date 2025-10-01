c46bf3edf008a248ea4c3799bb17594518330dda
rahamath45 <rahmathsulaiman45@gmail.com>
2025-10-01 23:17:06 +0530

    the last

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { createBooking } from "../api/booking";
 
